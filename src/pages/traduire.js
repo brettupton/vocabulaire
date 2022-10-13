18d5871b2249df92a2aacfb8c866814aef206a9d
Brett Upton <brettjupton@gmail.com>
2022-10-12 23:46:37 -0400

    heroku

--- src/pages/traduire.js
+++ src/pages/traduire.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import config from '../config'
+// import config from '../config'
 
 const speech = new SpeechSynthesisUtterance()
 const synth = window.speechSynthesis
