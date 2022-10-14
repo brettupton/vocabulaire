3765a3cb0e71c1e7dab55f624a6e8657ef7553ae
Brett Upton <brettjupton@gmail.com>
2022-10-13 21:07:02 -0400

    heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './index.css'
 import Layout from './pages/layout'
 import LesMots from './pages/lesmots'
 import WordFlashCard from './pages/wordflashcards'
