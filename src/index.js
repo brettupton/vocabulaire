693f77920b35fd3afc3f511dfde5f33357d9157b
Brett Upton <brettjupton@gmail.com>
2022-10-13 21:09:42 -0400

    heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Layout from './pages/layout'
 import LesMots from './pages/lesmots'
