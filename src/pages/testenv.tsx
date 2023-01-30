7639cf51053cadc31a479f7fa4d0a69624de3ed7
Brett Upton <brettjupton@gmail.com>
2023-01-29 23:54:45 -0500

    typescript added

--- src/pages/testenv.tsx
+++ src/pages/testenv.tsx
@@ -1,11 +1,20 @@
-
+type Word = {
+    French: string,
+    English: string,
+}
 
 export default function TestEnv() {
+
+    const word: Word = {
+        French: 'Bonjour',
+        English: 'Hello'
+    }
+
     return (
         <div className="container text-white text-center">
             <div className="row py-4">
                 <div className="col">
-                    TestEnv
+                    {word.French}
                 </div>
             </div>
         </div>
